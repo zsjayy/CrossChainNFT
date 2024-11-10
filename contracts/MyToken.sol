5833ccb4ed85a10ddbc27b329d99625f215641ce
jay <zsjayy@163.com>
2024-11-10 20:42:01 +0800

    commit crossChain first

---
+++ contracts/MyToken.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.22;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyToken is ERC721, ERC721Enumerable, ERC721Burnable, Ownable {
+    uint256 private _nextTokenId;
+    //metaData,有了这个之后，在铸造NFT的时候就可以用这个metaData的URI
+    string constant META_DATA = "ipfs://ipfs/QmNkvXzMDrw5RZxgUMYCPYriTpt9QKJC8FYPwnULucV2UV";
+
+    //通常合约的部署者就是Owner，所以可以直接用msg.sender，不用initialOwner
+    constructor(string memory _NFTName,string memory _NFTSymbol)
+        ERC721(_NFTName, _NFTSymbol)
+        Ownable(msg.sender)
+    {}
+
+    function safeMint(address to) public onlyOwner {
+        uint256 tokenId = _nextTokenId++;
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, META_DATA);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _update(address to, uint256 tokenId, address auth)
+        internal
+        override(ERC721, ERC721Enumerable)
+        returns (address)
+    {
+        return super._update(to, tokenId, auth);
+    }
+
+    function _increaseBalance(address account, uint128 value)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._increaseBalance(account, value);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
