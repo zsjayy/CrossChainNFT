72f84a93c25ccaf3d7ae16279f0aeff7e334180e
jay <zsjayy@163.com>
2024-11-20 19:11:14 +0800

    调试好mytoken.sol

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -8,7 +8,7 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MyToken is ERC721, ERC721Enumerable, ERC721Burnable, Ownable {
+contract MyToken is ERC721, ERC721Enumerable, ERC721URIStorage, ERC721Burnable, Ownable {
     uint256 private _nextTokenId;
     //metaData,有了这个之后，在铸造NFT的时候就可以用这个metaData的URI
     string constant META_DATA = "ipfs://ipfs/QmNkvXzMDrw5RZxgUMYCPYriTpt9QKJC8FYPwnULucV2UV";
@@ -42,10 +42,19 @@ contract MyToken is ERC721, ERC721Enumerable, ERC721Burnable, Ownable {
         super._increaseBalance(account, value);
     }
 
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
     function supportsInterface(bytes4 interfaceId)
         public
         view
-        override(ERC721, ERC721Enumerable)
+        override(ERC721, ERC721Enumerable, ERC721URIStorage)
         returns (bool)
     {
         return super.supportsInterface(interfaceId);
