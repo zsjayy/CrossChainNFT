9f83fab0f1e581acbd71998ce0a92e31c940e7f3
jay <zsjayy@163.com>
2024-11-23 16:17:57 +0800

    修改_buildCCIPMessage中Client.EVMExtraArgsV1，解决test脚本报错问题

--- contracts/NFTPoolBurnAndMint.sol
+++ contracts/NFTPoolBurnAndMint.sol
@@ -175,9 +175,9 @@ contract NFTPoolBurnAndMint is CCIPReceiver, OwnerIsCreator {
                     // Best Practice: For simplicity, the values are hardcoded. It is advisable to use a more dynamic approach
                     // where you set the extra arguments off-chain. This allows adaptation depending on the lanes, messages,
                     // and ensures compatibility with future CCIP upgrades. Read more about it here: https://docs.chain.link/ccip/best-practices#using-extraargs
-                    Client.EVMExtraArgsV2({
-                        gasLimit: 200_000, // Gas limit for the callback on the destination chain
-                        allowOutOfOrderExecution: true // Allows the message to be executed out of order relative to other messages from the same sender
+                    Client.EVMExtraArgsV1({
+                        gasLimit: 200_000 // Gas limit for the callback on the destination chain
+                        // allowOutOfOrderExecution: true // Allows the message to be executed out of order relative to other messages from the same sender
                     })
                 ),
                 // Set the feeToken to a feeTokenAddress, indicating specific asset will be used for fees
